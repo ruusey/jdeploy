e2f17fcc4b2f1586ab70144cbf0b0754d8f510d5
Robert Usey <ruusey@gmail.com>
2024-11-04 08:09:24 -0500

    Clean up methods

--- src/main/java/com/jdeploy/service/AuhenticatedUser.java
+++ src/main/java/com/jdeploy/service/AuhenticatedUser.java
@@ -12,94 +12,93 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Data
 public class AuhenticatedUser implements UserInfo, UIKeyboardInteractive {
-	private final static GridBagConstraints LAYOUT = new GridBagConstraints(0, 0, 1, 1, 1, 1,
-			GridBagConstraints.NORTHWEST, GridBagConstraints.NONE, new Insets(0, 0, 0, 0), 0, 0);
-	private String passwd;
-	private JTextField passwordField = (JTextField) new JPasswordField(20);
+    private final static GridBagConstraints LAYOUT = new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.NORTHWEST, GridBagConstraints.NONE,
+            new Insets(0, 0, 0, 0), 0, 0);
+    private String passwd;
+    private JTextField passwordField = (JTextField) new JPasswordField(20);
 
-	private Container panel;
+    private Container panel;
 
-	public AuhenticatedUser() {
-		log.info("Created new SSH auth context");
-	}
+    public AuhenticatedUser() {
+        log.info("Created new SSH auth context");
+    }
 
-	public String getPassword() {
-		return passwd;
-	}
+    public String getPassword() {
+        return passwd;
+    }
 
-	public boolean promptYesNo(String str) {
-		final Object[] options = { "yes", "no" };
-		final int foo = JOptionPane.showOptionDialog(null, str, "Warning", JOptionPane.DEFAULT_OPTION,
-				JOptionPane.WARNING_MESSAGE, null, options, options[0]);
-		return foo == 0;
-	}
+    public boolean promptYesNo(String str) {
+        final Object[] options = { "yes", "no" };
+        final int foo = JOptionPane.showOptionDialog(null, str, "Warning", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options,
+                options[0]);
+        return foo == 0;
+    }
 
-	public boolean promptPassword(String message) {
-		final Object[] ob = { passwordField };
-		final int result = JOptionPane.showConfirmDialog(null, ob, message, JOptionPane.OK_CANCEL_OPTION);
-		log.info("Prompting user for RSA password");
-		if (result == JOptionPane.OK_OPTION) {
-			this.passwd = this.passwordField.getText();
-			return true;
-		} else {
-			return false;
-		}
-	}
+    public boolean promptPassword(String message) {
+        final Object[] ob = { passwordField };
+        final int result = JOptionPane.showConfirmDialog(null, ob, message, JOptionPane.OK_CANCEL_OPTION);
+        log.info("Prompting user for RSA password");
+        if (result == JOptionPane.OK_OPTION) {
+            this.passwd = this.passwordField.getText();
+            return true;
+        } else {
+            return false;
+        }
+    }
 
-	public void showMessage(String message) {
-		JOptionPane.showMessageDialog(null, message);
-	}
+    public void showMessage(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
 
-	public String getPassphrase() {
-		return null;
-	}
+    public String getPassphrase() {
+        return null;
+    }
 
-	public boolean promptPassphrase(String message) {
-		return false;
-	}
+    public boolean promptPassphrase(String message) {
+        return false;
+    }
 
-	public String[] promptKeyboardInteractive(String destination, String name, String instruction, String[] prompt,
-			boolean[] echo) {
+    public String[] promptKeyboardInteractive(String destination, String name, String instruction, String[] prompt, boolean[] echo) {
 
-		this.panel = new JPanel();
-		this.panel.setLayout(new GridBagLayout());
+        this.panel = new JPanel();
+        this.panel.setLayout(new GridBagLayout());
 
-		LAYOUT.weightx = 1.0;
-		LAYOUT.gridwidth = GridBagConstraints.REMAINDER;
-		LAYOUT.gridx = 0;
-		this.panel.add(new JLabel(instruction), LAYOUT);
-		LAYOUT.gridy++;
+        LAYOUT.weightx = 1.0;
+        LAYOUT.gridwidth = GridBagConstraints.REMAINDER;
+        LAYOUT.gridx = 0;
+        this.panel.add(new JLabel(instruction), LAYOUT);
+        LAYOUT.gridy++;
 
-		LAYOUT.gridwidth = GridBagConstraints.RELATIVE;
+        LAYOUT.gridwidth = GridBagConstraints.RELATIVE;
 
-		final JTextField[] texts = new JTextField[prompt.length];
-		for (int i = 0; i < prompt.length; i++) {
-			LAYOUT.fill = GridBagConstraints.NONE;
-			LAYOUT.gridx = 0;
-			LAYOUT.weightx = 1;
-			this.panel.add(new JLabel(prompt[i]), LAYOUT);
+        final JTextField[] texts = new JTextField[prompt.length];
+        for (int i = 0; i < prompt.length; i++) {
+            LAYOUT.fill = GridBagConstraints.NONE;
+            LAYOUT.gridx = 0;
+            LAYOUT.weightx = 1;
+            this.panel.add(new JLabel(prompt[i]), LAYOUT);
 
-			LAYOUT.gridx = 1;
-			LAYOUT.fill = GridBagConstraints.HORIZONTAL;
-			LAYOUT.weighty = 1;
-			if (echo[i]) {
-				texts[i] = new JTextField(20);
-			} else {
-				texts[i] = new JPasswordField(20);
-			}
-			this.panel.add(texts[i], LAYOUT);
-			LAYOUT.gridy++;
-		}
+            LAYOUT.gridx = 1;
+            LAYOUT.fill = GridBagConstraints.HORIZONTAL;
+            LAYOUT.weighty = 1;
+            if (echo[i]) {
+                texts[i] = new JTextField(20);
+            } else {
+                texts[i] = new JPasswordField(20);
+            }
+            this.panel.add(texts[i], LAYOUT);
+            LAYOUT.gridy++;
+        }
 
-		if (JOptionPane.showConfirmDialog(null, this.panel, destination + ": " + name, JOptionPane.OK_CANCEL_OPTION,
-				JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) {
-			final String[] response = new String[prompt.length];
-			for (int i = 0; i < prompt.length; i++) {
-				response[i] = texts[i].getText();
-			}
-			return response;
-		} else {
-			return null;
-		}
-	}
+        if (JOptionPane.showConfirmDialog(null, this.panel, destination + ": " + name, JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.QUESTION_MESSAGE) == JOptionPane.OK_OPTION) {
+            final String[] response = new String[prompt.length];
+            for (int i = 0; i < prompt.length; i++) {
+                response[i] = texts[i].getText();
+            }
+            return response;
+        } else {
+            return null;
+        }
+    }
 }
--- src/main/java/com/jdeploy/service/DeploymentHelper.java
+++ src/main/java/com/jdeploy/service/DeploymentHelper.java
@@ -23,251 +23,250 @@ import com.jcraft.jsch.JSch;
 import com.jcraft.jsch.Session;
 
 import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 public class DeploymentHelper {
-	private Session session = null;
-	private Channel sftpChannel = null;
-	private Channel execChannel = null;
-	private Channel shellChannel = null;
+    private Session session = null;
+    private Channel sftpChannel = null;
+    private Channel execChannel = null;
+    private Channel shellChannel = null;
 
-	public DeploymentHelper(String remoteHost, Integer remotePort, String user, String password, String keyFile) {
-		final JSch jsch = new JSch();
-		try {
-			if (keyFile != null) {
-				jsch.addIdentity(keyFile);
-			}
-			this.session = jsch.getSession(user, remoteHost, remotePort);
-			if (password != null) {
-				this.session.setPassword(password);
-			}
-			final Properties config = new Properties();
-			config.put("StrictHostKeyChecking", "no");
-	        config.put("PreferredAuthentications", "publickey,gssapi-keyex,gssapi-with-mic,keyboard-interactive");
-	        config.put("UseDNS", "no");
-	        final UserInfo ui= new AuhenticatedUser();
-	        this.session.setUserInfo(ui);
-			this.session.setConfig(config);
-			this.session.connect(15000);
-			this.sftpChannel = this.session.openChannel("sftp");
-			this.sftpChannel.connect();
-			
-			this.shellChannel = this.session.openChannel("shell");
-			this.shellChannel.connect();
-			
-			
-			this.execChannel = this.session.openChannel("exec");
-			this.execChannel.connect();
+    public DeploymentHelper(String remoteHost, Integer remotePort, String user, String password, String keyFile) {
+        final JSch jsch = new JSch();
+        try {
+            if (keyFile != null) {
+                jsch.addIdentity(keyFile);
+            }
+            this.session = jsch.getSession(user, remoteHost, remotePort);
+            if (password != null) {
+                this.session.setPassword(password);
+            }
+            final Properties config = new Properties();
+            config.put("StrictHostKeyChecking", "no");
+            config.put("PreferredAuthentications", "publickey,gssapi-keyex,gssapi-with-mic,keyboard-interactive");
+            config.put("UseDNS", "no");
+            final UserInfo ui = new AuhenticatedUser();
+            this.session.setUserInfo(ui);
+            this.session.setConfig(config);
+            this.session.connect(15000);
+            this.sftpChannel = this.session.openChannel("sftp");
+            this.sftpChannel.connect();
 
-		} catch (Exception e) {
-			log.error("Failed to establish SSH session. Reason: {}", e);
-		}
-	}
-	
-	public ChannelSftp getSftpChannel() {
-		return ((ChannelSftp) this.sftpChannel);
-	}
-	
-	public ChannelExec getExecChannel() {
-		return ((ChannelExec) this.execChannel);
-	}
-	
-	public ChannelShell getShellChannel() {
-		return ((ChannelShell) this.shellChannel);
-	}
+            this.shellChannel = this.session.openChannel("shell");
+            this.shellChannel.connect();
 
-	public void termiateSession() {
-		try {
-			this.sftpChannel.disconnect();
-			this.session.disconnect();
-			this.execChannel.disconnect();
-		} catch (Exception e) {
-			log.error("Failed to clean up SSH resosurces. Reason: {}", e);
-		}
-	}
-	
-	public String readShell() {
-		String shellContent = null;
-		try {
-			InputStream input = this.shellChannel.getInputStream();
-			int size = input.available();
-			if(size>0) {
-				byte[] buffer = new byte[input.available()];
-				shellContent = new String(buffer, StandardCharsets.UTF_8);
-			}
-			
-		}catch(Exception e) {
-			log.error("Failed to read remote shell. Reason: {}", e);
-		}
-		return shellContent;
-	}
-	
-	public String readExec() {
-		String shellContent = null;
-		try {
-			final InputStream input = this.getExecChannel().getInputStream();
-			final int size = input.available();
-			if(size>0) {
-				final byte[] buffer = new byte[input.available()];
-				shellContent = new String(buffer, StandardCharsets.UTF_8);
-			}
-			
-		}catch(Exception e) {
-			log.error("Failed to read remote shell. Reason: {}", e);
-		}
-		return shellContent;
-	}
-	
-	public void writeShell(String command) {
-		try {
-			final OutputStream input = this.shellChannel.getOutputStream();
-			final PrintWriter writer = new PrintWriter(input);
-			writer.write(command);
-			writer.flush();
-			//writer.close();
-		}catch(Exception e) {
-			log.error("Failed to write remote shell. Reason: {}", e);
-		}
-	}
+            this.execChannel = this.session.openChannel("exec");
+            this.execChannel.connect();
 
-	// Exectutes two remote shell commands on the target box
-	// One to kill the existing processs
-	// Two to restart the new process
-	public void doKillRun(String sourceFile, String redirectOut) {
-		log.info("Performing kill and run of file {} with params {}", sourceFile, redirectOut);
-		final String[] fileParts = extractFileParts(sourceFile);
-		final String extraParams = redirectOut == null ? "" : "> " + fileParts[0] + "/" + redirectOut;
+        } catch (Exception e) {
+            log.error("Failed to establish SSH session. Reason: {}", e);
+        }
+    }
 
-		try {
-			final String[] commands = new String[] { "pkill -f " + fileParts[1],
-					"nohup java -jar " + sourceFile + " " + extraParams + "&" };
-			final long start = Instant.now().toEpochMilli();
-			for (String command : commands) {
-				this.doRun(command);
-				Thread.sleep(500);
-			}
-			log.info("Executed kill and run for file {}  in {}ms", sourceFile,
-					(Instant.now().toEpochMilli() - start));
-		} catch (Exception e) {
-			log.error("Failed to execute Kill and Run command. Reason: {}", e);
-		}
-	}
+    public ChannelSftp getSftpChannel() {
+        return ((ChannelSftp) this.sftpChannel);
+    }
 
-	public void doRun(String command) {
-		log.info("Performing run of command {} ", command);
-		try {
-			final long start = Instant.now().toEpochMilli();
-			this.execChannel = this.session.openChannel("exec");
-			log.info("Executing remote shell command '{}'", command);
-			this.getExecChannel().setCommand(command);
-			this.execChannel.connect();
-			this.execChannel.disconnect();
-			log.info("Executed run of command {}  in {}ms", command, (Instant.now().toEpochMilli() - start));
-		} catch (Exception e) {
-			log.error("Failed to execute Kill and Run command. Reason: {}", e);
-		}
-	}
+    public ChannelExec getExecChannel() {
+        return ((ChannelExec) this.execChannel);
+    }
 
-	// Sftps the target file path to the remote file location
-	public void doSftp(String sourceFile, String targetFile) {
-		try {
-			final File f = new File(sourceFile);
-			if (!f.exists()) {
-				throw new Exception("Source file " + sourceFile + " does not exist.");
-			}
-			// If the target file lies within a folder we need to CD to that folder
-			if (targetFile.indexOf("/") > -1) {
-				final String lastFolder = extractFileParts(targetFile)[0];
-				this.getSftpChannel().cd(lastFolder);
-			}
-			log.info("Storing file as remote filename: {} under path {}", extractFileParts(targetFile)[1],
-					extractFileParts(targetFile)[0]);
-			final long transferStart = Instant.now().toEpochMilli();
-			this.getSftpChannel().put(new java.io.FileInputStream(f), extractFileParts(targetFile)[1]);
-			log.info("SFTP transfer successful in {}ms", (Instant.now().toEpochMilli() - transferStart));
-		} catch (Exception e) {
-			log.error("Storing remote file failed. Reason: {}", e);
-		}
-	}
+    public ChannelShell getShellChannel() {
+        return ((ChannelShell) this.shellChannel);
+    }
 
-	public void deployAndRun(String sourceFile, String targetFile) {
-		try {
+    public void termiateSession() {
+        try {
+            this.sftpChannel.disconnect();
+            this.execChannel.disconnect();
+            this.shellChannel.disconnect();
+            this.session.disconnect();
+        } catch (Exception e) {
+            log.error("Failed to clean up SSH resosurces. Reason: {}", e);
+        }
+    }
+    
+    private String readChannel(Channel channel) {
+        String shellContent = null;
+        try {
+            final InputStream input = channel.getInputStream();
+            final int size = input.available();
+            if (size > 0) {
+                byte[] buffer = new byte[input.available()];
+                shellContent = new String(buffer, StandardCharsets.UTF_8);
+            }
 
-			final long start = Instant.now().toEpochMilli();
-			this.doSftp(sourceFile, targetFile);
-			this.doKillRun(targetFile, "service-log.out");
-			log.info("Deployment of file {} to remote directory {} completed in {}ms",
-					extractFileParts(sourceFile)[1], targetFile, (Instant.now().toEpochMilli() - start));
-			this.termiateSession();
-		} catch (Exception e) {
-			log.error("Failed to deploy file {}. Reason: {}", sourceFile, e);
-		}
-	}
+        } catch (Exception e) {
+            log.error("Failed to read remote shell. Reason: {}", e);
+        }
+        return shellContent;
+    }
 
-	public void deployAndExecuteCommand(String sourceFile, String targetFile, String commandToRun) {
-		try {
-			final long start = Instant.now().toEpochMilli();
-			this.doSftp(sourceFile, targetFile);
-			this.doRun(commandToRun);
-			log.info("Deployment of file {} to remote directory {} completed in {}ms",
-					extractFileParts(sourceFile)[1], targetFile, (Instant.now().toEpochMilli() - start));
-			this.termiateSession();
-		} catch (Exception e) {
-			log.error("Failed to deploy file {}. Reason: {}", sourceFile, e);
-		}
-	}
+    public String readShell() {
+       return this.readChannel(this.shellChannel);
+    }
 
-	// Returns an array of [directoriesFileIsIn, fileName]
-	public static String[] extractFileParts(String filePath) {
-		final int lastSlash = filePath.lastIndexOf("/");
-		if (lastSlash == -1) {
-			return new String[] { "", filePath };
-		}
-		final String directory = filePath.substring(0, lastSlash);
-		final String file = filePath.substring(lastSlash + 1);
-		return new String[] { directory, file };
-	}
+    public String readExec() {
+        return this.readChannel(this.execChannel);
+    }
+    
+    private void writeChannel(String command, Channel channel) {
+        try {
+            final OutputStream output = channel.getOutputStream();
+            final PrintWriter writer = new PrintWriter(output);
+            writer.write(command);
+            writer.flush();
+            writer.close();
+        } catch (Exception e) {
+            log.error("Failed to write remote shell. Reason: {}", e);
+        }
+    }
 
-	public static void main(String[] args) {
-		final CommandLineParser parser = new DefaultParser();
-		final Options options = new Options();
-		options.addOption("h", "remote-host", true, "Remote host");
-		options.addOption("p", "remote-port", true, "Remote port (usually 22)");
-		options.addOption("u", "remote-username", true, "Remote login username");
-		options.addOption("y", "remote-user-password", true, "Remote login passsword");
-		options.addOption("i", "remote-private-key", true, "Remote host privvate key");
-		options.addOption("s", "source-file", true, "Local file path to deploy");
-		options.addOption("t", "target-file", true, "Remote file path to deploy to");
+    public void writeShell(String command) {
+       this.writeChannel(command, this.shellChannel);
+    }
+    
+    public void writeExec(String command) {
+        this.writeChannel(command, this.execChannel);
+     }
 
-		if ((args.length > 0) && args[0].contains("spring.output.ansi")) {
-			args[0] = "";
-		}
-		try {
-			final CommandLine cmd = parser.parse(options, args);
+    // Exectutes two remote shell commands on the target box
+    // One to kill the existing processs
+    // Two to restart the new process
+    public void doKillRun(String sourceFile, String redirectOut) {
+        log.info("Performing kill and run of file {} with params {}", sourceFile, redirectOut);
+        final String[] fileParts = extractFileParts(sourceFile);
+        final String extraParams = redirectOut == null ? "" : "> " + fileParts[0] + "/" + redirectOut;
 
-			if (cmd.hasOption("help")) {
-				final HelpFormatter formatter = new HelpFormatter();
-				formatter.printHelp("DeploymentHelper v0.1", options, true);
-				System.exit(-1);
-			}
-			final String host = cmd.getOptionValue("remote-host");
-			final String port = cmd.getOptionValue("remote-port");
+        try {
+            final String[] commands = new String[] { "pkill -f " + fileParts[1], "nohup java -jar " + sourceFile + " " + extraParams + "&" };
+            final long start = Instant.now().toEpochMilli();
+            for (String command : commands) {
+                this.doRun(command);
+                Thread.sleep(500);
+            }
+            log.info("Executed kill and run for file {}  in {}ms", sourceFile, (Instant.now().toEpochMilli() - start));
+        } catch (Exception e) {
+            log.error("Failed to execute Kill and Run command. Reason: {}", e);
+        }
+    }
 
-			final String user = cmd.getOptionValue("remote-username");
-			final String password = cmd.getOptionValue("remote-user-password");
-			final String key = cmd.getOptionValue("remote-private-key");
+    public void doRun(String command) {
+        log.info("Performing run of command {} ", command);
+        try {
+            final long start = Instant.now().toEpochMilli();
+            this.execChannel = this.session.openChannel("exec");
+            log.info("Executing remote shell command '{}'", command);
+            this.getExecChannel().setCommand(command);
+            this.execChannel.connect();
+            this.execChannel.disconnect();
+            log.info("Executed run of command {}  in {}ms", command, (Instant.now().toEpochMilli() - start));
+        } catch (Exception e) {
+            log.error("Failed to execute Kill and Run command. Reason: {}", e);
+        }
+    }
 
-			final String sourceFile = cmd.getOptionValue("source-file");
-			final String targetFile = cmd.getOptionValue("target-file");
-			final DeploymentHelper helper = new DeploymentHelper(host, Integer.parseInt(port), user, password, key);
-			helper.doSftp(sourceFile ,targetFile);
-			helper.doRun("chmod 777 "+targetFile);
-			System.out.println("NEXT STEPS:");
-			System.out.println("su upscapi");
-			System.out.println("/opt/UPS/portalservice/script/deploy{Service}.sh");
-			System.out.println("/opt/UPS/apiservice/script/deploy{Service}.sh");
-			helper.termiateSession();
-		} catch (Exception e) {
-			log.error("Failed to parse command line arguments. Reason: {}", e);
-		}
-	}
+    // Sftps the target file path to the remote file location
+    public void doSftp(String sourceFile, String targetFile) {
+        try {
+            final File f = new File(sourceFile);
+            if (!f.exists()) {
+                throw new Exception("Source file " + sourceFile + " does not exist.");
+            }
+            // If the target file lies within a folder we need to CD to that folder
+            if (targetFile.indexOf("/") > -1) {
+                final String lastFolder = extractFileParts(targetFile)[0];
+                this.getSftpChannel().cd(lastFolder);
+            }
+            log.info("Storing file as remote filename: {} under path {}", extractFileParts(targetFile)[1], extractFileParts(targetFile)[0]);
+            final long transferStart = Instant.now().toEpochMilli();
+            this.getSftpChannel().put(new java.io.FileInputStream(f), extractFileParts(targetFile)[1]);
+            log.info("SFTP transfer successful in {}ms", (Instant.now().toEpochMilli() - transferStart));
+        } catch (Exception e) {
+            log.error("Storing remote file failed. Reason: {}", e);
+        }
+    }
+
+    public void deployAndRun(String sourceFile, String targetFile) {
+        try {
+
+            final long start = Instant.now().toEpochMilli();
+            this.doSftp(sourceFile, targetFile);
+            this.doKillRun(targetFile, "service-log.out");
+            log.info("Deployment of file {} to remote directory {} completed in {}ms", extractFileParts(sourceFile)[1], targetFile,
+                    (Instant.now().toEpochMilli() - start));
+            this.termiateSession();
+        } catch (Exception e) {
+            log.error("Failed to deploy file {}. Reason: {}", sourceFile, e);
+        }
+    }
+
+    public void deployAndExecuteCommand(String sourceFile, String targetFile, String commandToRun) {
+        try {
+            final long start = Instant.now().toEpochMilli();
+            this.doSftp(sourceFile, targetFile);
+            this.doRun(commandToRun);
+            log.info("Deployment of file {} to remote directory {} completed in {}ms", extractFileParts(sourceFile)[1], targetFile,
+                    (Instant.now().toEpochMilli() - start));
+            this.termiateSession();
+        } catch (Exception e) {
+            log.error("Failed to deploy file {}. Reason: {}", sourceFile, e);
+        }
+    }
+
+    // Returns an array of [directoriesFileIsIn, fileName]
+    public static String[] extractFileParts(String filePath) {
+        final int lastSlash = filePath.lastIndexOf("/");
+        if (lastSlash == -1) {
+            return new String[] { "", filePath };
+        }
+        final String directory = filePath.substring(0, lastSlash);
+        final String file = filePath.substring(lastSlash + 1);
+        return new String[] { directory, file };
+    }
+
+    public static void main(String[] args) {
+        final CommandLineParser parser = new DefaultParser();
+        final Options options = new Options();
+        options.addOption("h", "remote-host", true, "Remote host");
+        options.addOption("p", "remote-port", true, "Remote port (usually 22)");
+        options.addOption("u", "remote-username", true, "Remote login username");
+        options.addOption("y", "remote-user-password", true, "Remote login passsword");
+        options.addOption("i", "remote-private-key", true, "Remote host privvate key");
+        options.addOption("s", "source-file", true, "Local file path to deploy");
+        options.addOption("t", "target-file", true, "Remote file path to deploy to");
+
+        if ((args.length > 0) && args[0].contains("spring.output.ansi")) {
+            args[0] = "";
+        }
+        try {
+            final CommandLine cmd = parser.parse(options, args);
+
+            if (cmd.hasOption("help")) {
+                final HelpFormatter formatter = new HelpFormatter();
+                formatter.printHelp("DeploymentHelper v0.1", options, true);
+                System.exit(-1);
+            }
+            final String host = cmd.getOptionValue("remote-host");
+            final String port = cmd.getOptionValue("remote-port");
+
+            final String user = cmd.getOptionValue("remote-username");
+            final String password = cmd.getOptionValue("remote-user-password");
+            final String key = cmd.getOptionValue("remote-private-key");
+
+            final String sourceFile = cmd.getOptionValue("source-file");
+            final String targetFile = cmd.getOptionValue("target-file");
+            final DeploymentHelper helper = new DeploymentHelper(host, Integer.parseInt(port), user, password, key);
+            helper.deployAndRun(sourceFile, targetFile);
+//			helper.doSftp(sourceFile ,targetFile);
+//			helper.doRun("chmod 777 "+targetFile);
+//			System.out.println("NEXT STEPS:");
+//			System.out.println("su upscapi");
+//			System.out.println("/opt/UPS/portalservice/script/deploy{Service}.sh");
+//			System.out.println("/opt/UPS/apiservice/script/deploy{Service}.sh");
+//			helper.termiateSession();
+        } catch (Exception e) {
+            log.error("Failed to parse command line arguments. Reason: {}", e);
+        }
+    }
 }
